789fa384416b2f1cf1977ca5ea77dd1b510f1cf9
Joe Peck <joe.peck@scimedsolutions.com>
2016-01-09 12:57:57 -0500

    Get rid of some scaffoled comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,58 +1,50 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
   def create
     @post = Post.new(post_params)
 
     if @post.save
       redirect_to @post, notice: 'Post was successfully created.'
     else
-      render :new
+      render :new, alert: 'Post could not be created.'
     end
   end
 
-  # PATCH/PUT /posts/1
   def update
     if @post.update(post_params)
       redirect_to @post, notice: 'Post was successfully updated.'
     else
-      render :edit
+      render :edit, alert: 'Post could not be updated.'
     end
   end
 
-  # DELETE /posts/1
   def destroy
     @post.destroy
     redirect_to posts_url, notice: 'Post was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def post_params
-      params.require(:post).permit(:title, :description)
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 end
