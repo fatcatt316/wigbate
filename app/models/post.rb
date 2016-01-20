49c0a0b1b4690ccd2e50cd19256052bf6b853d88
Joe Peck <joe.peck@scimedsolutions.com>
2016-01-19 20:28:30 -0500

    Add and use slugs for Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,28 @@
 class Post < ActiveRecord::Base
   mount_uploader :comic, ComicUploader
 
+  after_create :set_slug
+
   validates :title, presence: true, uniqueness: { case_sensitive: false }
   validates :comic, presence: true
+  validates :slug, presence: true, uniqueness: { case_sensitive: false }, if: :persisted?
 
   has_many :comments, dependent: :destroy
 
   def self.random(exclude_id: nil)
     where.not(id: exclude_id).order('RANDOM()').first
+  end
+
+  def to_param
+    slug
+  end
+
+  private
+
+  def set_slug
+    if slug.blank?
+      self.slug = "#{id}-#{title.parameterize}"
+      self.save!
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110021408) do
+ActiveRecord::Schema.define(version: 20160116034145) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160110021408) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "comic"
+    t.string   "slug"
   end
 
 end
