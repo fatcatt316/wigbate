9f774676018fc7328c96d1698dceca04be1da046
Joe Peck <joe.peck@scimedsolutions.com>
2016-01-19 20:35:33 -0500

    Improve Comment error messages

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   validates :post, presence: true
-  validates :author, presence: true
-  validates :body, presence: true
+  validates :author, presence: { message: 'has to be filled in!' }
+  validates :body, presence: { message: "can't just be blank."}
 
   belongs_to :post
 
