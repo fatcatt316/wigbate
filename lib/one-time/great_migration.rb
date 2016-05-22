da0bc15c83c7bd7211b9170d5ebcb9e002b4a7aa
Joe Peck <joe.peck@scimedsolutions.com>
2016-05-21 21:43:57 -0400

    Migrator basically working... except multi-comic pages :-/

--- lib/one-time/great_migration.rb
+++ lib/one-time/great_migration.rb
@@ -5,62 +5,74 @@ module GreatMigration
 
   START_URL = 'http://www.wigbate.com/1---joker.html'
 
+  # TODO: Figure out multi-image posts (e.g., 'http://www.wigbate.com/458---462---pigs-1-7.html')
+
   def run
+    original_logger_level = Rails.logger.level
+    Rails.logger.level = :warn
+
     browser = Watir::Browser.start(START_URL)
 
-    i = 0 # TODO: Remove
-    # while(true) # TODO: Not this
-    while(i < 3)
+    while(true) # TODO: Not this
       import_post!(browser)
 
       next_link = browser.span(text: 'NEXT').try(:parent)
       break unless next_link
       next_link.click # Loads new browser page
-      i += 1
     end
 
     puts "Done!"
+  ensure
+    Rails.logger.level = original_logger_level
   end
-
-  # def copy_comments
-  #   browser = Watir::Browser.start(START_URL)
-
-  #   comment_divs = browser.divs(class: 'comment')
-
-  # end
 
   private
 
   def import_post!(browser)
     url = browser.url
-    puts "Copying #{url}..."
     page = Nokogiri::HTML.parse(browser.html)
+    puts "Copying #{url}..."
 
-    Post.create!(
+    post = Post.create!(
       title: page_title(page),
       slug: page_slug(url),
+      description: comic_description(page),
       remote_comic_url: comic_image_url(page, browser)
     )
 
-    # TODO: Import comments
-  rescue => e
-    debugger
-    puts "WHY"
+    import_comments!(post, page)
+  end
+
+  def import_comments!(post, page)
+    comments = page.css('div#comments_list').css('div.comment')
+    puts "Importing #{comments.size} comments..."
+
+    comments.each do |comment|
+      post.comments.create!(
+        author: comment.css('span.author b').text.strip,
+        body: comment.css('blockquote').text.strip,
+        created_at: comment_date(comment)
+      )
+    end
+  end
+
+  def comic_description(page)
+    description = page.css('.wsite-image').css('div').last.text
+    description if description.present?
+  end
+
+  def comment_date(comment) # E.g., (Feb 27, 2013)
+    Date.parse(comment.css('span.date').text)
   end
 
   def comic_image_url(page, browser)
     comic_url = page.css('img').map{|img| img['src']}.find do |url| url.start_with?('/uploads/') && !url.end_with?('1427127701.png') end
     return "http://www.wigbate.com#{comic_url}" if comic_url
 
-    # else, have to find it through background image
+    # ... or, have to find it through background image
     # E.g., "url(\"http://www.wigbate.com/uploads/1/2/4/3/12434276/header_images/1390837983.jpg\")"
     image_css = browser.div(class: 'wsite-header').style('background-image')
-    if image_css and m = image_css.match(/(http:\/\/www\.wigbate\.com\/uploads.+\.jpg)/)
-      return m[1]
-    end
-
-    debugger
-    puts "Where is the comic???"
+    image_css.match(/(http:\/\/www\.wigbate\.com\/uploads.+\.jpg)/)[1]
   end
 
   def page_title(page)
@@ -72,52 +84,4 @@ module GreatMigration
   def page_slug(url)
     url.sub("http://www.wigbate.com/",'')
   end
-
-  # Mechanize
-  # def import_post!(page)
-  #   post = Post.create!(
-  #     title: page_title(page),
-  #     slug: page.uri.to_s.sub("http://www.wigbate.com/",''),
-  #     remote_comic_url: comic_image_url(page)
-  #   )
-  # rescue => e
-  #   debugger
-  #   puts "WHY"
-  # end
-
-  # Mechanize
-  # def comic_image_url(page)
-  #   # If it has div.wsite-image, use the inner img src
-  #   comic_url = page.image_urls.map(&:to_s).find do |url|
-  #     url.start_with?('http://www.wigbate.com/uploads/') && !url.end_with?('1427127701.png')
-  #   end
-  #   return comic_url if comic_url
-  # end
-
-  # E.g., "1 - Joker - wigbate - updated triweekly"
-  # Mechanize
-  # def page_title(page)
-  #   page.title.to_s
-  #     .sub(/\d+ - /, '')
-  #     .sub(' - wigbate - updated triweekly','')
-  # end
-
-  # Mechanize
-  # def run
-  #   mechanize = Mechanize.new
-  #   mechanize.history_added = Proc.new { sleep 0.5 } # rate limiting
-  #   page = mechanize.get(START_URL)
-
-  #   # Go through them and copy 'em
-  #   i = 0
-  #   while(page)
-  #     i += 1
-  #     puts "Copying #{page.uri}..."
-  #     import_post!(page)
-  #     next_link = page.links.find{|link| link.text == "\r\nNEXT\r\n"}
-  #     page = next_link.try(:click)
-  #     break if i >= 10 # TODO: Remove
-  #   end
-  #   puts "Done!"
-  # end
 end
