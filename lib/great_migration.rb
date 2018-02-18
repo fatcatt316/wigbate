a86cd503a46ffdd34800516418794359e97147df
Joe Peck <fatcatt316@yahoo.com>
2018-02-18 10:24:19 -0500

    Move GreatMigration into lib

--- lib/great_migration.rb
+++ lib/great_migration.rb
@@ -3,32 +3,34 @@ require 'watir'
 module GreatMigration
   extend self
 
-  START_URL = 'http://www.wigbate.com/1---joker.html'
+  START_URL = 'https://www.wigbate.com/1---joker.html'
+  HEADER_IMAGE = '1427127701.png'
 
   # TODO: Figure out multi-image posts (e.g., 'http://www.wigbate.com/458---462---pigs-1-7.html')
 
-  def run
+  def run(start_url: START_URL, limit: nil)
     original_logger_level = Rails.logger.level
     Rails.logger.level = :warn
+    import_count = 0
 
-    browser = Watir::Browser.start(START_URL)
+    browser = Watir::Browser.start(start_url)
 
     while(true) # TODO: Not this
+      break if limit && import_count > limit
       import_post!(browser)
 
       next_link = browser.span(text: 'NEXT').try(:parent)
       break unless next_link
+      import_count += 1
       next_link.click # Loads new browser page
     end
 
-    puts "Done!"
+    puts 'Done!'
   ensure
     Rails.logger.level = original_logger_level
   end
 
-  private
-
-  def import_post!(browser)
+  private def import_post!(browser)
     url = browser.url
     page = Nokogiri::HTML.parse(browser.html)
     puts "Copying #{url}..."
@@ -37,13 +39,13 @@ module GreatMigration
       title: page_title(page),
       slug: page_slug(url),
       description: comic_description(page),
-      remote_comic_url: comic_image_url(page, browser)
+      remote_comics_urls: comic_image_urls(page, browser)
     )
 
     import_comments!(post, page)
   end
 
-  def import_comments!(post, page)
+  private def import_comments!(post, page)
     comments = page.css('div#comments_list').css('div.comment')
     puts "Importing #{comments.size} comments..."
 
@@ -56,32 +58,44 @@ module GreatMigration
     end
   end
 
-  def comic_description(page)
-    description = page.css('.wsite-image').css('div').last.text
-    description if description.present?
+  private def comic_description(page)
+    page.css('.wsite-image').css('div').last&.text
   end
 
-  def comment_date(comment) # E.g., (Feb 27, 2013)
+  private def comment_date(comment) # E.g., (Feb 27, 2013)
     Date.parse(comment.css('span.date').text)
   end
 
-  def comic_image_url(page, browser)
-    comic_url = page.css('img').map{|img| img['src']}.find do |url| url.start_with?('/uploads/') && !url.end_with?('1427127701.png') end
-    return "http://www.wigbate.com#{comic_url}" if comic_url
+  private def comic_image_urls(page, browser)
+    urls = page.css('img')
+      .map { |img| "https://www.wigbate.com#{img['src']}" if valid_comic_url?(img['src']) }
+      .compact
 
-    # ... or, have to find it through background image
-    # E.g., "url(\"http://www.wigbate.com/uploads/1/2/4/3/12434276/header_images/1390837983.jpg\")"
-    image_css = browser.div(class: 'wsite-header').style('background-image')
-    image_css.match(/(http:\/\/www\.wigbate\.com\/uploads.+\.jpg)/)[1]
+    urls + background_image_urls(browser)
   end
 
-  def page_title(page)
+  # Add images from background
+  # E.g., "url(\"http://www.wigbate.com/uploads/1/2/4/3/12434276/header_images/1390837983.jpg\")"
+  private def background_image_urls(browser)
+    image_css = browser.div(class: 'wsite-header').style('background-image')
+    if image_css == 'none'
+      []
+    else
+      [image_css.match(/(https:\/\/www\.wigbate\.com\/uploads.+\.jpg)/)[1]]
+    end
+  end
+
+  private def page_title(page)
     page.css('title').text
       .sub(/\d+ - /, '')
       .sub(' - wigbate - updated triweekly','')
   end
 
-  def page_slug(url)
-    url.sub("http://www.wigbate.com/",'')
+  private def page_slug(url)
+    url.sub('https://www.wigbate.com/', '')
+  end
+
+  private def valid_comic_url?(url)
+    url.start_with?('/uploads/') && !url.end_with?(HEADER_IMAGE)
   end
 end
